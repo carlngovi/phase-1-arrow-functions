8b34ab71b97756ae5e490bde634631795dce7932
Carl Ngovi <carl.ngovi@student.moringaschool.com>
2024-03-21 06:56:03 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide (){
+   let a = 2000;
+   let b = 100;
+   return a/b;
+}
+
+let x=2;
+const square = (x) => x*x;
+
+let num1 =3;
+let num2 =4;
+const add = (num1, num2) => num1+num2;
